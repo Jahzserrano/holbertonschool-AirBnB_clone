a93495416716fbcd827dc616ce8358b866e70ad0
leomar <leomar.rodriguez1@upr.edu>
2024-02-28 16:28:16 -0400

    pushing changes in pycodestyle

--- console.py
+++ console.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-
+"""Defining HBNBCommand class"""
 import cmd
 
-"""Defining HBNBCommand class"""
 
 class HBNBCommand(cmd.Cmd):
     """Custom command interpreter for HBNB project."""
-    
+
     prompt = "(hbnb) "
-    
+
     def do_quit(self, arg):
         """Exit the program."""
         return True
-    
+
     def do_EOF(self, arg):
         """Exit the program on EOF (Ctrl-D)."""
         print("\nGoodbye!")
         return True
-    
+
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
