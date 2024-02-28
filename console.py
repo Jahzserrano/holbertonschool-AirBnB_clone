ec5111b390dc657a562dddee2822c07aa2d2ed68
leomar <leomar.rodriguez1@upr.edu>
2024-02-28 17:37:12 -0400

    pushing fixed in base_model pycodestyle

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Defining HBNBCommand class"""
 import cmd
+import json
+
+from numpy import save
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Custom command interpreter for HBNB project."""
 
     prompt = "(hbnb) "
+    valid_classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Exit the program."""
@@ -20,6 +25,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel and save it to the JSON file."""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+    
+    def show(self, arg):
+        """Prints the string representation of an instance"""
+        if not arg:
+            print("** class name missing **") 
 
 
 if __name__ == "__main__":
