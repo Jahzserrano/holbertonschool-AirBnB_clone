3d8a85a6a4490b9eea15517c94d3767119e6735f
leomar <leomar.rodriguez1@upr.edu>
2024-02-29 13:58:32 -0400

    deleted classes that are not implemented yet

--- console.py
+++ console.py
@@ -5,12 +5,7 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,12 +14,6 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     __classes = {
         "BaseModel": BaseModel,
-        "User": User,
-        "State": State,
-        "City": City,
-        "Place": Place,
-        "Amenity": Amenity,
-        "Review": Review
         }
 
     def emptyline(self):
