976efa22e9bdc6a296296082e4ec751a028b69fd
leomar <leomar.rodriguez1@upr.edu>
2024-03-01 15:20:27 -0400

    changes in user class to test checkers

--- models/user.py
+++ models/user.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""Defining class User"""
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel."""
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
